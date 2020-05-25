d5f2f7ba60b8dfcea21470414529cae298978e3f
Garnett05 <charlleslimamota10@hotmail.com>
2020-05-25 16:41:32 -0300

    Created a unit test

---
+++ ToDo.Tests/BoolToTextDecorationConverterTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using ToDo;
+using AppToDo.Services;
+using Xamarin.Forms;
+
+namespace ToDo.Tests
+{
+    public class BoolToTextDecorationConverterTests
+    {
+        [Fact]
+        public void Convert_returns_strikethrough_when_item_is_completed()
+        {
+            //Arrange
+            var conveter = new BoolToTextDecorationConverter();
+            //Act
+            var result = conveter.Convert(true, null, null, null);
+            //Assert
+            Assert.Equal(TextDecorations.Strikethrough, (TextDecorations)result);
+        }
+        [Fact]
+        public void Convert_returns_strikethrough_when_item_is_not_completed()
+        {
+            //Arrange
+            var conveter = new BoolToTextDecorationConverter();
+            //Act
+            var result = conveter.Convert(false, null, null, null);
+            //Assert
+            Assert.Equal(TextDecorations.None, (TextDecorations)result);
+        }
+    }
+}
---
+++ ToDo.Tests/ToDoViewModelTests.cs
@@ -0,0 +1,43 @@
+﻿using AppToDo.ViewModels;
+using AppToDo.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace ToDo.Tests
+{
+    public class ToDoViewModelTests
+    {
+        [Fact]
+        public void ViewModel_populates_items_correctly()
+        {
+            //Arrange
+            ToDoViewModel viewModel = new ToDoViewModel();
+            //Act
+            //Assert
+            Assert.Equal(5, viewModel.Items.Count);
+        }
+        [Fact]
+        public void AddItemCommand_adds_new_item_to_the_list()
+        {
+            //Arrange
+            ToDoViewModel viewModel = new ToDoViewModel();
+            //Act
+            viewModel.AddItemCommand.Execute(null);
+            //Assert
+            Assert.Equal(6, viewModel.Items.Count);
+            Assert.Equal("ToDo Item 6", viewModel.Items.Last().Name);
+        }
+        [Fact]
+        public void MarkAsCompletedCommand_updates_progress()
+        {
+            var viewModel = new ToDoViewModel();
+            viewModel.MarkAsCompletedCommand.Execute(viewModel.Items.First());
+
+            Assert.Equal("Completed 1/5", viewModel.CompletedHeader);
+            Assert.Equal(0.2, viewModel.CompletedProgress);
+        }
+    }
+}
